df0853ec55e2f191a2cdd04750aecf7cf9fc5330
Constantin Craciun <madaerodog@users.noreply.github.com>
2017-10-29 21:49:40 +0200

    Update fresh_install.sh

--- fresh_install.sh
+++ fresh_install.sh
@@ -8,7 +8,10 @@ uname -a
 sudo apt-get update
 sudo apt-get -y upgrade
 
+# Linux basic stuff
+sudo apt-get install -y terminator
+
 # Development Tools
-sudo apt-get install -y build-essential git nodejs
+sudo apt-get install -y build-essential git nodejs python3-pip virtualenv
 # Media Tools
 sudo apt-get install -y vlc handbrake gimp inkscape feh blender
