f73628021be8ec141858bd5d987af69b4af993ac
Constantin Craciun <madaerodog@users.noreply.github.com>
2017-10-29 22:16:55 +0200

    Update fresh_install.sh

--- fresh_install.sh
+++ fresh_install.sh
@@ -12,6 +12,16 @@ sudo apt-get -y upgrade
 sudo apt-get install -y terminator
 
 # Development Tools
-sudo apt-get install -y build-essential git nodejs python3-pip virtualenv
+sudo apt-get install -y build-essential git nodejs npm python3-pip virtualenv
+
+# Upgrade nodejs to latest stable version
+sudo npm cache clean -f
+sudo npm install -g n
+sudo n stable
+
+# you also need to give this command after
+# sudo ln -sf /usr/local/n/versions/node/<VERSION>/bin/node /usr/bin/nodejs
+
+
 # Media Tools
 sudo apt-get install -y vlc handbrake gimp inkscape feh blender
