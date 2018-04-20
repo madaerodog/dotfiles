0b2022e05d80de08f8ec63fe64baa26e79208e50
Constantin Craciun <madaerodog@users.noreply.github.com>
2018-04-20 10:37:34 +0300

    Update fresh_install.sh

--- fresh_install.sh
+++ fresh_install.sh
@@ -6,15 +6,12 @@
 cat /etc/*-release
 uname -a
 
-# Add usefull PPAs
-sudo add-apt-repository -y ppa:stebbins/handbrake-releases
-
 # Upgrade packages that are available to update
 sudo apt-get update
 sudo apt-get -y upgrade
 
 # Linux basic stuff
-sudo apt-get install -y chromium-browser terminator gparted
+sudo apt-get install -y chromium-browser terminator zsh gparted
 
 # Create basic folders
 mkdir Development
@@ -28,12 +25,12 @@ sudo npm cache clean -f
 sudo npm install -g n
 sudo n stable
 
-# you also need to give this command after
+# you also need to run this command after
 # sudo ln -sf /usr/local/n/versions/node/<VERSION>/bin/node /usr/bin/nodejs
 
 
 # Media Tools
-sudo apt-get install -y vlc handbrake-gtk gimp inkscape feh blender
+sudo apt-get install -y vlc gimp inkscape feh blender
 
 
 # i3 wm
@@ -44,13 +41,6 @@ sudo npm install -g i3-style
 sudo apt-get install -y android-tools-adb
 sudo apt-get install -y android-tools-fastboot
 sudo usermod -aG plugdev $LOGNAME
-
-
-# gekko
-cd Development
-git clone git://github.com/askmike/gekko.git
-cd gekko
-npm install --only=production
 
 # git settings
 git config --global push.default simple
